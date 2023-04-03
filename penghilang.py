d3744479e6979bed735463e9fd55c6321a71f988
AdiNurFiYan <adikembar0683@gmail.com>
2023-04-03 16:55:18 +0700

    update2

---
+++ penghilang.py
@@ -0,0 +1,6 @@
+def show_menu():
+    print("Menu:")
+    print("1. Kopi         Rp10.000")
+    print("2. Teh          Rp7.000")
+    print("3. Jus          Rp15.000")
+    print("4. Air Mineral  Rp5.000")
