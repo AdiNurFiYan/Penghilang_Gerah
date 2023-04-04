31b0378ecfba4cb685eed3bbca921d1ee51827b8
BBILLKN <billytekkom22@gmail.com>
2023-04-04 12:45:12 +0700

    Update ke 2

--- penghilang.py
+++ penghilang.py
@@ -4,3 +4,23 @@ def show_menu():
     print("2. Teh          Rp7.000")
     print("3. Jus          Rp15.000")
     print("4. Air Mineral  Rp5.000")
+
+def order_drink():
+    show_menu()
+    choice = int(input("Pilihan Anda: "))
+    quantity = int(input("Jumlah: "))
+    if choice == 1:
+        price = 10000
+        drink = "Kopi"
+    elif choice == 2:
+        price = 7000
+        drink = "Teh"
+    elif choice == 3:
+        price = 15000
+        drink = "Jus"
+    elif choice == 4:
+        price = 5000
+        drink = "Air Mineral"
+    else:
+        print("Maaf, pilihan tidak tersedia.")
+        return
